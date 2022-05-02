b70d98269fa0b4ea4861331ed86fb656e938c5cd
Akshaysaini5381 <akshaysaini5381@gmail.com>
2022-05-02 23:56:10 +0530

    Add project files.

---
+++ character and digit find/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace character_and_digit_find
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the Input :  ");
+         
+            char data = Convert.ToChar(Console.ReadLine());
+
+            Console.WriteLine(" this a value {0} ",data);
+            if (data >= 65 && data<=90 || data >= 97 && data <= 122)
+                Console.WriteLine("this a character   ");
+
+           /* else if(sam>=97 && sam<=122)
+                Console.WriteLine(" this a samol  character");*/
+            else
+                Console.WriteLine(" this is a digit  ");
+            Console.ReadLine();
+
+
+        }
+    }
+}
